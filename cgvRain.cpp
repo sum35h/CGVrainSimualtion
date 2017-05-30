692564a37290a340fa39d4fbac25ea07969d8283
Sumesh Pradhan <sumesh.6361@gmail.com>
2017-05-30 09:57:12 +0530

    Added low polygon trees, reformatted the existing code

--- cgvRain.cpp
+++ cgvRain.cpp
@@ -19,38 +19,29 @@ float zoom = -40.0;
 float pan = 0.0;
 float tilt = 0.0;
 float hailsize = 0.05;
-
+int loop;
+int fall;
+float angle = 0.0f;
+float lx=0.0f,lz=-1.0f;
+int precipitation=0;
+float x=0.0f, z=5.0f;
+float deltaAngle = 0.0f;
+float deltaMove = 0;
+int xOrigin = -1;
 void initRendering() {
 	glEnable(GL_DEPTH_TEST);
 //	glEnable(GL_LIGHTING);
 //	glEnable(GL_LIGHT0);
 //	glEnable(GL_NORMALIZE);
 	glEnable(GL_COLOR_MATERIAL);
-//	 glEnable(GL_FOG);
-
+//	glEnable(GL_FOG);
 }
 
-int loop;
-int fall;
-//angle of rotation for the camera direction
-float angle = 0.0f;
-
-// actual vector representing the camera's direction
-float lx=0.0f,lz=-1.0f;
-int precipitation=0;
-// XZ position of the camera
-float x=0.0f, z=5.0f;
-
-// the key states,These variables will be zero
-//when no key is being presses
-float deltaAngle = 0.0f;
-float deltaMove = 0;
-int xOrigin = -1;
 typedef struct {
   // Life
-  bool alive;	// is the particle alive?
+  bool alive;	// checks the particle alive
   float life;	// particle lifespan
-  float fade; // decay(for hail only)
+  float fade; // decay
   // color
   float red;
   float green;
@@ -59,7 +50,7 @@ typedef struct {
   float xpos;
   float ypos;
   float zpos;
-  // Velocity/Direction, only goes down in y dir
+  // Velocity
   float vel;
   // Gravity
   float gravity;
@@ -68,7 +59,7 @@ typedef struct {
 // Paticle System
 particles par_sys[MAX_PARTICLES];
 int rain_vel=3;
-// Initialize/Reset Particles - give them their attributes
+// Initialize/Reset Particles 
 void initParticles(int i) {
     par_sys[i].alive = true;
     par_sys[i].life = 3;
@@ -86,26 +77,10 @@ void initParticles(int i) {
     par_sys[i].gravity = -5;//-0.8;
 
 }
-particles par_sys2[MAX_PARTICLES];
+
 particles par_sys3[MAX_PARTICLES];
-void initParticlesMat(int i,particles par_sys[MAX_PARTICLES],int p,int q,int r,int s) {
-    par_sys[i].alive = true;
-    par_sys[i].life = 3;
-    par_sys[i].fade = float(rand()%100)/1000.0f+0.003f;
 
-    par_sys[i].xpos = (float) (rand() % q+p) - 10;
-    par_sys[i].ypos = 50;
-    par_sys[i].zpos = (float) (rand() % s+r) - 10;
-
-    par_sys[i].red = 0.5;
-    par_sys[i].green = 0.5;
-    par_sys[i].blue = 1.0;
-
-    par_sys[i].vel = rain_vel;
-    par_sys[i].gravity = -3;//-0.8;
-
-}
-void initParticlesMatNegative(int i,particles par_sys[MAX_PARTICLES],int p,int q,int r,int s) {
+void initParticlesMatNegative(int i,particles par_sys[MAX_PARTICLES]) {
     par_sys[i].alive = true;
     par_sys[i].life = 3;
     par_sys[i].fade = float(rand()%100)/1000.0f+0.003f;
@@ -124,44 +99,40 @@ par_sys[i].zpos=-1*par_sys[i].zpos ;
 
 }
 void changeSize(int w, int h) {
-
-	// Prevent a divide by zero, when window is too short
-	
 	if (h == 0)
 		h = 1;
-
-	float ratio =  w * 1.0 / h;
-
-	// Use the Projection Matrix
-	glMatrixMode(GL_PROJECTION);
-
-	// Reset Matrix
-	glLoadIdentity();
-
-	// Set the viewport to be the entire window
-	glViewport(0, 0, w, h);
-
-	// Set the correct perspective.
-	gluPerspective(45.0f, ratio, 0.1f, 100.0f);
-
-	// Get Back to the Modelview
-	glMatrixMode(GL_MODELVIEW);
+    float ratio =  w * 1.0 / h;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, w, h);
+    gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+    glMatrixMode(GL_MODELVIEW);
 }
 
-void drawbox() {
+void drawTree() {
 
-	glColor3f(0.4f, 0.4f, 1.0f);
+    glPushMatrix();
+	glColor3f(0.6,0.6,0); 
+    glTranslatef(0.1f ,0.5f, 0.0f);
+	glScalef(1,2.5,1);
+	glutSolidCube(1);
+	glPopMatrix();
 
-// Draw Body
-	glTranslatef(0.0f ,0.75f, 0.0f);
-	//glutSolidSphere(0.75f,20,20);
-//	glutSolidCube(2);
-	//glutSolidTetrahedron();
+    glPushMatrix();
+	glColor3f(0,1,0.4); 
+	glScalef(1.8,1.7,1.8);
+	
+   //glTranslatef(0.0f ,1.5f, 0.0f);
+   //glutSolidIcosahedron();
+	glTranslatef(0.0f ,2.4f, 0.0f);
+    glutSolidDodecahedron();
+	glPopMatrix();
+
+    //glutSolidTetrahedron();
 	//glutSolidIcosahedron();
-		//glTranslatef(0.0f ,1.0f, 0.0f);
-	glutSolidOctahedron();
-//glutSolidDodecahedron();
-//glutSolidTeapot(2);
+    //glutSolidOctahedron();
+  //glutSolidDodecahedron();
+   //glutSolidTeapot(2);
 
 }
 
@@ -187,12 +158,9 @@ void drawRain() {
         glVertex3f(x, y+0.5, z);
       glEnd();
 
-      // Update values
-      //Move
-      // Adjust slowdown for speed
       par_sys[loop].ypos += par_sys[loop].vel / (slowdown*1000);
       par_sys[loop].vel += par_sys[loop].gravity;
-      // Decay
+    
       par_sys[loop].life -= par_sys[loop].fade;
 
       if (par_sys[loop].ypos <= -10) {
@@ -206,52 +174,6 @@ void drawRain() {
   }
 }
 
-void drawRainMat(particles par_sys2[]) {
-  float x, y, z;
-  for (loop = 0; loop < MAX_PARTICLES; loop=loop+2) {
-    if (par_sys2[loop].alive == true) {
-      x = par_sys2[loop].xpos*-1;
-      y = par_sys2[loop].ypos;
-      z = par_sys2[loop].zpos*-1 + zoom;
-
-      // Draw particles
-      glColor3f(0.5, 0.5, 1.0);
-      glBegin(GL_LINES);
-        glVertex3f(x, y, z);
-        glVertex3f(x, y+0.5, z);
-      
-      glEnd();
-
-      // Update values
-      //Move
-      // Adjust slowdown for speed!
-      par_sys2[loop].ypos += par_sys2[loop].vel / (slowdown*1000);
-      par_sys2[loop].vel += par_sys2[loop].gravity;
-      // Decay
-      par_sys2[loop].life -= par_sys2[loop].fade;
-
-      if (par_sys2[loop].ypos <= -10) {
-        par_sys2[loop].life = -1.0;
-      }
-      //Revive
-       if (par_sys[loop].ypos <= -10) {
-        int zi = z - zoom + 10;
-        int xi = x + 10;
-        ground_colors[zi][xi][0] = 1.0;
-        ground_colors[zi][xi][2] = 1.0;
-        ground_colors[zi][xi][3] += 1.0;
-        if (ground_colors[zi][xi][3] > 1.0) {
-          ground_points[xi][zi][1] += 0.1;
-        }
-        par_sys[loop].life = -1.0;
-      }
-
-      if (par_sys2[loop].life < 0.0) {
-        initParticlesMat(loop,par_sys2,0,200,100,200);
-      }
-    }
-  }
-}
 void drawRainMatNegative(particles par_sys2[]) {
   float x, y, z;
   for (loop = 0; loop < MAX_PARTICLES; loop=loop+2) {
@@ -265,15 +187,12 @@ void drawRainMatNegative(particles par_sys2[]) {
       glBegin(GL_LINES);
         glVertex3f(x, y, z);
         glVertex3f(x, y+0.5, z);
-     
-      glEnd();
+     glEnd();
 
-      // Update values
-      //Move
-      // Adjust slowdown for speed!
+    
       par_sys2[loop].ypos += par_sys2[loop].vel / (slowdown*1000);
       par_sys2[loop].vel += par_sys2[loop].gravity;
-      // Decay
+     
       par_sys2[loop].life -= par_sys2[loop].fade;
 
       if (par_sys2[loop].ypos <= -10) {
@@ -293,74 +212,13 @@ void drawRainMatNegative(particles par_sys2[]) {
       }
 
       if (par_sys2[loop].life < 0.0) {
-        initParticlesMatNegative(loop,par_sys3,0,100,0,100);
+        initParticlesMatNegative(loop,par_sys3);
       }
     }
   }
 }
 
-void drawHail() {
-  float x, y, z;
 
-  for (loop = 0; loop < MAX_PARTICLES; loop=loop+2) {
-    if (par_sys2[loop].alive == true) {
-      x = par_sys2[loop].xpos;
-      y = par_sys2[loop].ypos;
-      z = par_sys2[loop].zpos + zoom;
-
-      // Draw particles
-      glColor3f(0.8, 0.8, 0.9);
-      glBegin(GL_QUADS);
-        // Front
-        glVertex3f(x-hailsize, y-hailsize, z+hailsize); // lower left
-        glVertex3f(x-hailsize, y+hailsize, z+hailsize); // upper left
-        glVertex3f(x+hailsize, y+hailsize, z+hailsize); // upper right
-        glVertex3f(x+hailsize, y-hailsize, z+hailsize); // lower left
-        //Left
-        glVertex3f(x-hailsize, y-hailsize, z+hailsize);
-        glVertex3f(x-hailsize, y-hailsize, z-hailsize);
-        glVertex3f(x-hailsize, y+hailsize, z-hailsize);
-        glVertex3f(x-hailsize, y+hailsize, z+hailsize);
-        // Back
-        glVertex3f(x-hailsize, y-hailsize, z-hailsize);
-        glVertex3f(x-hailsize, y+hailsize, z-hailsize);
-        glVertex3f(x+hailsize, y+hailsize, z-hailsize);
-        glVertex3f(x+hailsize, y-hailsize, z-hailsize);
-        //Right
-        glVertex3f(x+hailsize, y+hailsize, z+hailsize);
-        glVertex3f(x+hailsize, y+hailsize, z-hailsize);
-        glVertex3f(x+hailsize, y-hailsize, z-hailsize);
-        glVertex3f(x+hailsize, y-hailsize, z+hailsize);
-        //Top
-        glVertex3f(x-hailsize, y+hailsize, z+hailsize);
-        glVertex3f(x-hailsize, y+hailsize, z-hailsize);
-        glVertex3f(x+hailsize, y+hailsize, z-hailsize);
-        glVertex3f(x+hailsize, y+hailsize, z+hailsize);
-        //Bottom
-        glVertex3f(x-hailsize, y-hailsize, z+hailsize);
-        glVertex3f(x-hailsize, y-hailsize, z-hailsize);
-        glVertex3f(x+hailsize, y-hailsize, z-hailsize);
-        glVertex3f(x+hailsize, y-hailsize, z+hailsize);
-      glEnd();
-
-      // Update values
-      //Move
-      if (par_sys2[loop].ypos <= -10) {
-        par_sys2[loop].vel = par_sys2[loop].vel * -1;
-      }
-      par_sys2[loop].ypos += par_sys2[loop].vel / (slowdown*1000); // * 1000
-      par_sys2[loop].vel += par_sys2[loop].gravity;
-
-      // Decay
-      par_sys2[loop].life -= par_sys2[loop].fade;
-
-      //Revive
-//      if (par_sys2[loop].life < 0.0) {
-//         initParticlesMat(loop,par_sys2);
-//      }
-    }
-  }
-}
 int fogOn=0;//enable ,disable fog
 int lightOn=0;//enable ,disable light
 
@@ -391,47 +249,35 @@ void renderScene(void) {
 	if (deltaMove)
 		computePos(deltaMove);
 
-	// Clear Color and Depth Buffers
+	
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// Reset transformations
 	glLoadIdentity();
 	// Set the camera
 	gluLookAt(	x, 1.0f, z,
 			x+lx, 1.0f,  z+lz,
 			0.0f, 1.0f,  0.0f);
 
-// Draw ground
-
-	
-	glBegin(GL_QUADS);
+   
+   
+   // ground 
+    glBegin(GL_QUADS);
 	glColor3f(0.1f, 1, .7f);
-//		glVertex3f(-100.0f, 0.0f, -100.0f);
-//		glVertex3f(-100.0f, 0.0f,  100.0f);
-//		glVertex3f( 100.0f, 0.0f,  100.0f);
-//		glVertex3f( 100.0f, 0.0f, -100.0f);
 
-	
-		
-//	  glColor3f(0.0f,100.0f,0.0f);    // Color Blue
-//    glVertex3f( 1000000.0f, 10000.0f,-10.0f);    // Top Right Of The Quad (Top)
-//    glVertex3f(-1000000.0f, 10000.0f,-10.0f);    // Top Left Of The Quad (Top)
-//    glVertex3f(-1000000.0f, 10000.0f, 10.0f);    // Bottom Left Of The Quad (Top)
-//    glVertex3f( 10000.0f, 10000.0f, 10.0f);    // Bottom Right Of The Quad (Top)
-   // glColor3f(1.0f,0.5f,0.0f);    // Color Orange
    	glColor3f(0.1f, 1, .7f);
     glVertex3f( 100.0f,0, 100.0f);    // Top Right Of The Quad (Bottom)
     glVertex3f(-100.0f,0, 100.0f);    // Top Left Of The Quad (Bottom)
     glVertex3f(-100.0f,0,-100.0f);    // Bottom Left Of The Quad (Bottom)
     glVertex3f( 100.0f,0,-100.0f);    // Bottom Right Of The Quad (Bottom)
     
-    glColor3f(0.0f,15.0f,100.0f); 
-	//glColor3f(1.0f,0.0f,0.0f);    // Color Red    
+    //SKYBOX
+    glColor3f(0.0f,15.0f,100.0f); //Sky color
+   
     glVertex3f( 100000.0f, 10000.0f, 50.0f);    // Top Right Of The Quad (Front)
     glVertex3f(-100000.0f, 10000.0f, 50.0f);    // Top Left Of The Quad (Front)
     glVertex3f(-100000.0f,-10000.0f, 50.0f);    // Bottom Left Of The Quad (Front)
     glVertex3f( 100000.0f,-10000.0f,50.0f);    // Bottom Right Of The Quad (Front)
-    //glColor3f(1.0f,1.0f,0.0f);    // Color Yellow
+ 
     glColor4f(0.0f,15.0f,100.0f,0); 
 	glVertex3f( 10000.0f,-10000.0f,-50.0f);    // Top Right Of The Quad (Back)
     glVertex3f(-10000.0f,-10000.000f,-50.0f);    // Top Left Of The Quad (Back)
@@ -442,7 +288,7 @@ void renderScene(void) {
     glVertex3f(-50.0f, 10000.0f,-10000.0f);    // Top Left Of The Quad (Left)
     glVertex3f(-50.0f,-10000.0f,-10000.0f);    // Bottom Left Of The Quad (Left)
     glVertex3f(-50.0f,-10000.0f, 10000.0f);    // Bottom Right Of The Quad (Left)
-    //glColor3f(100.0f,0.0f,100.0f);    // Color Violet
+  
     glColor4f(0.0f,15.0f,100.0f,0); 
 	glVertex3f( 50.0f, 10000.0f,-10000.0f);    // Top Right Of The Quad (Right)
     glVertex3f( 50.0f, 10000.0f, 10000.0f);    // Top Left Of The Quad (Right)
@@ -451,14 +297,14 @@ void renderScene(void) {
 	glEnd();
 
 
-
+/////Object(TREE) generation in Grid 9x9 
 	for(int i = -3; i < 3; i++)
 		for(int j=-3; j < 3; j++) {
 	
                 glPushMatrix();
                 
                     glTranslatef(i*10.0,0,j * 10.0);
-                     drawbox();
+                     drawTree();
                     glPopMatrix();
                }
                
@@ -468,38 +314,16 @@ void renderScene(void) {
                 //No Precipitation
                 break;
                 case 1:
-                //drawRain();
-                //drawRainMat(par_sys2);
                 drawRainMatNegative(par_sys3);
-                  drawRain();
-                break;
-                case 2:
-                //drawHail();
                 drawRain();
                 break;
+              
                }
-//              drawRain();
-//              //drawRain2();
-//                drawHail();
+
         glutSwapBuffers();
 } 
 
-void processNormalKeys(unsigned char key, int xx, int yy) { 	
 
-        if (key == 27)
-              exit(0);
-              
-          if(key=='s')
-		   precipitation=0;
-		   
-		      if(key=='r')
-		   precipitation=1;
-		   
-		      if(key=='h')
-		   precipitation=2;
-		       
-              
-} 
 
 void pressKey(int key, int xx, int yy) {
 
@@ -519,34 +343,31 @@ void releaseKey(int key, int x, int y) {
 
 void mouseMove(int x, int y) { 	
 
-         // this will only be true when the left button is down
+     
          if (xOrigin >= 0) {
 
-		// update deltaAngle
+		
 		deltaAngle = (x - xOrigin) * 0.001f;
 
-		// update camera's direction
+		//Camera direction
 		lx = sin(angle + deltaAngle);
 		lz = -cos(angle + deltaAngle);
 	}
 }
 
-void mouseButton(int button, int state, int x, int y) {
-
-	// only start motion if the left button is pressed
+void mouseButton(int button, int state, int x, int y) 
+{
 	if (button == GLUT_LEFT_BUTTON) {
-
-		// when the button is released
 		if (state == GLUT_UP) {
 			angle += deltaAngle;
 			xOrigin = -1;
 		}
-		else  {// state = GLUT_DOWN
+		else  {
 			xOrigin = x;
 		}
 	}
 }
-//////////////
+
 void menu(int id)
 {
 	if(id==0)
@@ -560,11 +381,7 @@ void menu_effect(int id)
 {
 	if(id==1)
 	{
-//		glEnable(GL_DEPTH_TEST);
-//	glEnable(GL_LIGHTING);
-//	glEnable(GL_LIGHT0);
-//	glEnable(GL_NORMALIZE);
-//	glEnable(GL_COLOR_MATERIAL);
+
 if(fogOn==1)
 {
 	 glDisable(GL_FOG);
@@ -612,10 +429,10 @@ void menu_rain(int id)
 	}
 	glutPostRedisplay();
 }
-////////////
+
 int main(int argc, char **argv) {
 
-	// init GLUT and create window
+	
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(420,320);
@@ -623,9 +440,9 @@ int main(int argc, char **argv) {
 	
 	glutCreateWindow("CGV-Rain Project");
    for (loop = 0; loop < MAX_PARTICLES; loop++) {
-        initParticlesMat(loop,par_sys2,0,200,100,200);
+   
         initParticles(loop);
-        initParticlesMatNegative(loop,par_sys3,1,200,0,100);
+        initParticlesMatNegative(loop,par_sys3);
     }
 	initRendering();
 	
@@ -634,15 +451,15 @@ int main(int argc, char **argv) {
 	glutIdleFunc(renderScene);
 
 	glutIgnoreKeyRepeat(1);
-	glutKeyboardFunc(processNormalKeys);
+	
 	glutSpecialFunc(pressKey);
 	glutSpecialUpFunc(releaseKey);
 
-	//mouse events
+
 	glutMouseFunc(mouseButton);
 	glutMotionFunc(mouseMove);
-/////////////menu/////////////////Will bind the options later....
-int rain_ch=glutCreateMenu(menu_rain);
+
+     int rain_ch=glutCreateMenu(menu_rain);
     glutAddMenuEntry("start ",0);
     glutAddMenuEntry("stop ",1);
 	glutAddMenuEntry("increase velocity",2);
@@ -653,8 +470,8 @@ int rain_ch=glutCreateMenu(menu_rain);
     	glutCreateMenu(menu);
     	
 		int effect_ch=glutCreateMenu(menu_effect);
-        glutAddMenuEntry("Fog",1);
-    	glutAddMenuEntry("Lighting",2);
+        glutAddMenuEntry("Enable/Disable Fog",1);
+    	glutAddMenuEntry("Enble/Disable Lighting",2);
     	
     glutCreateMenu(menu);
     glutAddSubMenu("Precipitation",prec_ch);
@@ -665,11 +482,10 @@ int rain_ch=glutCreateMenu(menu_rain);
 	  
 	  glutAttachMenu(GLUT_RIGHT_BUTTON);
 
-//////////////////////////////
-	// OpenGL init
+
 	glEnable(GL_DEPTH_TEST);
 
-	// enter GLUT event processing cycle
+	
 	glutMainLoop();
 
 	return 1;
